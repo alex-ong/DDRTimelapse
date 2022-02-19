33646836f2b320873ef001045052e90fa6f13a1e
alex-ong <alex.ong@unsw.edu.au>
2022-02-20 09:59:07 +1100

    fix: Bug with multiple python installations

--- renderAudioVideo.bat
+++ renderAudioVideo.bat
@@ -1,5 +1,5 @@
 ffmpeg -i %1.mp4 -vn -b:a 320k %1.wav
-call parseVideo.py %1
-call parseAudio.py %1
+call python parseVideo.py %1
+call python parseAudio.py %1
 ffmpeg -i %1_processed.wav -r 60 -i %1/%%05d.png %1_processed.mp4
 copy %1_processed.mp4 \\savooby-desktop\ddr\%1_processed.mp4
--- renderAudioVideoNoPad.bat
+++ renderAudioVideoNoPad.bat
@@ -1,4 +1,4 @@
 ffmpeg -i %1.mp4 -vn -b:a 320k %1.wav
-call parseVideo.py %1 nopad
-call parseAudio.py %1
+call python parseVideo.py %1 nopad
+call python parseAudio.py %1
 ffmpeg -i %1_processed.wav -r 60 -i %1/%%05d.png %1_processed.mp4
